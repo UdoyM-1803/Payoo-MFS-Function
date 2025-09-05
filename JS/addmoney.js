2135e8b8d1066f46cb1d995a51f28e6e291f986e
UdoyM-1803 <udoymunna17@gmail.com>
2025-09-05 17:50:16 +0600

    Added function in new javascript file and connect it with html

---
+++ JS/addmoney.js
@@ -0,0 +1,12 @@
+
+console.log('Add money Loaded');
+
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event) {
+        event.preventDefault();
+
+        // const addMoney = document.getElementById('input-add-money').value;
+        // const addMoneyNumber = parseFloat(addMoney);
+
+
+    }) 
---
+++ JS/utilities.js
@@ -0,0 +1,9 @@
+/**
+ * Common Shared functions here
+ * */ 
+
+console.log('Utilities Loaded')
+
+function getInputFieldValueById() {
+    console.log('We will get value by id');
+}

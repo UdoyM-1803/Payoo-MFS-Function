5bd66b7453e60e6429a6d00a1652db3855aa9875
UdoyM-1803 <udoymunna17@gmail.com>
2025-09-06 00:04:21 +0600

    Made sure the Cash out section will work successfully

--- JS/cashout.js
+++ JS/cashout.js
@@ -4,4 +4,13 @@ document.getElementById('btn-cash-out')
 
         const cashOut = getInputFieldValueById('cash-out-amount');
         const pinNumber = getInputFieldValueById('cash-out-pin');
+
+        if (pinNumber === 1234) {
+            const balance = getTextFieldValueById('account-balance');
+            const newBalance = balance - cashOut;
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else {
+            alert('No  money for you..... DGM');
+        }
     })
